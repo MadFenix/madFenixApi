443153c99b1147a4843e27eddc772909bb118783
Valenti Gamez <vgr.gamez@gmail.com>
2024-02-03 19:10:23 +0100

    Fixed email delete account.

--- resources/views/emails/deleteAccount.blade.php
+++ resources/views/emails/deleteAccount.blade.php
@@ -1,33 +1,15 @@
-<x-mail::layout>
-    {{-- Header --}}
-    <x-slot:header>
-        <x-mail::header :url="config('app.url')">
-            {{ config('app.name') }}
-        </x-mail::header>
-    </x-slot:header>
-
-    {{-- Body --}}
-    <p>Un usuario se quiere dar de baja:</p>
-    <ul>
-        <li>Id: {{ $userId }}</li>
-        <li>Username: {{ $userName }}</li>
-        <li>Email: {{ $userEmail }}</li>
-        <li>Token: {{ $lastToken }}</li>
-    </ul>
-
-    {{-- Subcopy --}}
-    @isset($subcopy)
-        <x-slot:subcopy>
-            <x-mail::subcopy>
-                {{ $subcopy }}
-            </x-mail::subcopy>
-        </x-slot:subcopy>
-    @endisset
-
-    {{-- Footer --}}
-    <x-slot:footer>
-        <x-mail::footer>
-            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
-        </x-mail::footer>
-    </x-slot:footer>
-</x-mail::layout>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mad Fénix delete account</title>
+</head>
+<body>
+<p>Un usuario se quiere dar de baja:</p>
+<ul>
+    <li>Id: {{ $userId }}</li>
+    <li>Username: {{ $userName }}</li>
+    <li>Email: {{ $userEmail }}</li>
+    <li>Token: {{ $lastToken }}</li>
+</ul>
+</body>
+</html>
