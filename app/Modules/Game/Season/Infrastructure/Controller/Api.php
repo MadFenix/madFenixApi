9ecaf56a3a4a9242a6c00f28dd057a8b7ee358a7
Valenti Gamez <vgr.gamez@gmail.com>
2024-04-24 12:22:07 +0200

    Added season functionality.

--- app/Modules/Game/Season/Infrastructure/Controller/Api.php
+++ app/Modules/Game/Season/Infrastructure/Controller/Api.php
@@ -46,6 +46,7 @@ class Api extends ResourceController
         $seasonDetails = (object) $activeSeason->toArray();
 
         $seasonRewards = SeasonReward::where('season_id', '=', $activeSeason->id)
+            ->orderByAsc('level')
             ->get();
         if ($seasonRewards->count() <= 0) {
             return response()->json('No se ha encontrado las recompensas de la season.', 404);
