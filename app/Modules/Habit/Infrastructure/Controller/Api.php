4f54eb0be1dc81c6c2cba9988e62ade2ce0a5f79
Valenti Gamez <vgr.gamez@gmail.com>
2024-05-06 12:59:45 +0200

    Added habits system.

--- app/Modules/Habit/Infrastructure/Controller/Api.php
+++ app/Modules/Habit/Infrastructure/Controller/Api.php
@@ -5,10 +5,13 @@ namespace App\Modules\Habit\Infrastructure\Controller;
 
 use App\Http\Controllers\Controller;
 use App\Modules\Game\Profile\Domain\Profile;
+use App\Modules\Game\Season\Domain\Season;
+use App\Modules\Game\Season\Domain\SeasonReward;
 use App\Modules\Habit\Domain\Habit;
 use App\Modules\Habit\Domain\HabitComplete;
 use App\Modules\Habit\Infrastructure\ApiHelix;
 use App\Modules\User\Domain\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class Api extends Controller
@@ -57,6 +60,43 @@ class Api extends Controller
         $habitComplete->habit_id = $habit->id;
 
         $habitCompleteSaved = $habitComplete->save();
+
+        $userHabits = Habit::where('user_id', '=', $user->id)->orderBy('order')->get();
+        $userHabitIds = [];
+        foreach ($userHabits as $userHabit) {
+            $userHabitIds[] = $userHabit->id;
+        }
+        $dateNow = Carbon::now();
+        $dateNow->startOfDay();
+        $userHabitCompletes = HabitComplete::where('created_at', '>', $dateNow->format('Y-m-d H:i:s'))->whereIn('habit_id', $userHabitIds)->get();
+        $userHabitCompletedIds = [];
+        foreach ($userHabitCompletes as $userHabitComplete) {
+            $userHabitCompletedIds[] = $userHabitComplete->habit_id;
+        }
+        if (count($userHabitIds) == count($userHabitCompletedIds)) {
+            $profile = Profile::where('user_id', '=', $user->id)->first();
+            if (!$profile) {
+                return response()->json('Perfil del usuario no encontrado.', 404);
+            }
+
+            $profile->season_points += 25000;
+
+            $dateNow = Carbon::now();
+            $activeSeason = Season::where('start_date', '<', $dateNow->format('Y-m-d H:i:s'))
+                ->where('end_date', '>', $dateNow->format('Y-m-d H:i:s'))
+                ->first();
+            if ($activeSeason) {
+                $lastSeasonReward = SeasonReward::where('season_id', '=', $activeSeason->id)
+                    ->where('required_points', '<', $profile->season_points)
+                    ->orderByDesc('level')
+                    ->first();
+                if ($lastSeasonReward) {
+                    $profile->season_level = $lastSeasonReward->level;
+                }
+            }
+
+            $profile->save();
+        }
 
         return $habitCompleteSaved
             ? response()->json('Hábito completado del usuario guardado.')
