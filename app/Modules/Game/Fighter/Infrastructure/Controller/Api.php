c3b8e59c7c70419e8b4dd3b13b8a5e6c18e8c223
Valenti Gamez <vgr.gamez@gmail.com>
2024-08-30 18:07:08 +0200

    Updated friend endpoints.

--- app/Modules/Game/Fighter/Infrastructure/Controller/Api.php
+++ app/Modules/Game/Fighter/Infrastructure/Controller/Api.php
@@ -342,7 +342,7 @@ class Api extends ResourceController
 
         $returnFighterUsers = [];
         foreach ($fighterUsers as $fighterUser) {
-            $user = User::where('id', $fighterUsers->user_id)->first();
+            $user = User::where('id', $fighterUser->user_id)->first();
             $newFighterUser = new \stdClass();
 
             $newFighterUser->user_id = $user->id;
