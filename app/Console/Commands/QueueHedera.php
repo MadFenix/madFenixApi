2844497501951759b52f196191ffceb67c3c5978
Valenti Gamez <vgr.gamez@gmail.com>
2025-01-08 16:34:05 +0100

    MAD-91 Added transaction fail in hedera queue to nfts without token id.

--- app/Console/Commands/QueueHedera.php
+++ app/Console/Commands/QueueHedera.php
@@ -113,51 +113,62 @@ class QueueHedera extends Command
 
             if ($messageQueueHedera->nft_identification_id > 0 && empty($messageQueueHedera->transaction_id)) {
                 $nftIdentification = $messageQueueHedera->nft_identification;
-                if (empty($nftIdentification)) {
+                $error = false;
+                if (empty($nftIdentification) && !$error) {
+                    $error = true;
                     $messageQueueHedera->transaction_id = 'Error';
                     $messageQueueHedera->attempts += 1;
                     $messageQueueHedera->save();
                 }
                 $nft = $nftIdentification->nft;
-                if (empty($nft)) {
+                if (empty($nft) && !$error) {
+                    $error = true;
                     $messageQueueHedera->transaction_id = 'Error';
                     $messageQueueHedera->attempts += 1;
                     $messageQueueHedera->save();
                 }
-                $this->line('Run hedera transfer');
-                $this->line('User id: ' . $messageQueueHedera->user_id);
-                $this->line('Id hedera: ' . $messageQueueHedera->id_hedera);
-                $this->line('NFT: ' . $nft->short_description . ' - ' . $nft->token_props . '.' . $nft->token_realm . '.' . $nft->token_number . ' - ' . $nftIdentification->nft_identification);
-                $process = new Process([$node, base_path() . '/node_scripts/hedera_transfer_nft_out.cjs', 'receiver_account_id=' . $messageQueueHedera->id_hedera, 'token_props=' . $nft->token_props, 'token_realm=' . $nft->token_realm, 'token_number=' . $nft->token_number, 'nft_identification=' . $nftIdentification->nft_identification, 'queue_hedera_id=' . $messageQueueHedera->id]);
-
-                try {
-                    $process->run(function ($type, $buffer):void {
-                        if (Process::ERR === $type) {
-                            $this->line('ERR > '.$buffer);
-                        }
-                    });
-                } catch (\Exception $e) {
-                    $this->line($e->getMessage());
-                }
-
-                $buffer = $process->getOutput();
-                $this->line($buffer);
-                $arguments = explode("\n", $buffer);
-                if (count($arguments) >= 3 && $arguments[0] == 'SUCCESS') {
-                    $transactionId = trim($arguments[2]);
-                    $messageQueueHedera->transaction_id = $transactionId;
-                    $messageQueueHedera->attempts += 1;
-                    $messageQueueHedera->done = true;
-                    $messageQueueHedera->save();
-                } elseif (count($arguments) >= 3 && $arguments[0] == 'FAIL') {
-                    $transactionId = trim($arguments[2]);
-                    $messageQueueHedera->transaction_id = $transactionId;
-                    $messageQueueHedera->attempts += 1;
-                    $messageQueueHedera->save();
-                } else {
+                if ($nft->token_number <= 0 && !$error) {
+                    $error = true;
                     $messageQueueHedera->transaction_id = 'Error';
                     $messageQueueHedera->attempts += 1;
                     $messageQueueHedera->save();
+                }
+                if (!$error) {
+                    $this->line('Run hedera transfer');
+                    $this->line('User id: ' . $messageQueueHedera->user_id);
+                    $this->line('Id hedera: ' . $messageQueueHedera->id_hedera);
+                    $this->line('NFT: ' . $nft->short_description . ' - ' . $nft->token_props . '.' . $nft->token_realm . '.' . $nft->token_number . ' - ' . $nftIdentification->nft_identification);
+                    $process = new Process([$node, base_path() . '/node_scripts/hedera_transfer_nft_out.cjs', 'receiver_account_id=' . $messageQueueHedera->id_hedera, 'token_props=' . $nft->token_props, 'token_realm=' . $nft->token_realm, 'token_number=' . $nft->token_number, 'nft_identification=' . $nftIdentification->nft_identification, 'queue_hedera_id=' . $messageQueueHedera->id]);
+
+                    try {
+                        $process->run(function ($type, $buffer):void {
+                            if (Process::ERR === $type) {
+                                $this->line('ERR > '.$buffer);
+                            }
+                        });
+                    } catch (\Exception $e) {
+                        $this->line($e->getMessage());
+                    }
+
+                    $buffer = $process->getOutput();
+                    $this->line($buffer);
+                    $arguments = explode("\n", $buffer);
+                    if (count($arguments) >= 3 && $arguments[0] == 'SUCCESS') {
+                        $transactionId = trim($arguments[2]);
+                        $messageQueueHedera->transaction_id = $transactionId;
+                        $messageQueueHedera->attempts += 1;
+                        $messageQueueHedera->done = true;
+                        $messageQueueHedera->save();
+                    } elseif (count($arguments) >= 3 && $arguments[0] == 'FAIL') {
+                        $transactionId = trim($arguments[2]);
+                        $messageQueueHedera->transaction_id = $transactionId;
+                        $messageQueueHedera->attempts += 1;
+                        $messageQueueHedera->save();
+                    } else {
+                        $messageQueueHedera->transaction_id = 'Error';
+                        $messageQueueHedera->attempts += 1;
+                        $messageQueueHedera->save();
+                    }
                 }
             }
         }
