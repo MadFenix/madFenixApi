217e1ca5f24810198b6bf4e3cbf929de69cd4e03
Valenti Gamez <vgr.gamez@gmail.com>
2025-04-05 19:48:20 +0200

    MAD-297 Añadido en la api los endpoints para controlar las encuestas.

--- app/Modules/Game/Season/Infrastructure/Controller/Api.php
+++ app/Modules/Game/Season/Infrastructure/Controller/Api.php
@@ -14,7 +14,7 @@ use App\Modules\Game\Ranking\Domain\Tournament;
 use App\Modules\Game\Season\Domain\Season;
 use App\Modules\Game\Season\Domain\SeasonReward;
 use App\Modules\Game\Season\Domain\SeasonRewardRedeemed;
-use App\Modules\Game\Season\Infrastructure\Service\PollUtilities;
+use App\Modules\Game\Season\Infrastructure\Service\UserSeasonPremium;
 use App\Modules\User\Domain\User;
 use Carbon\Carbon;
 use Illuminate\Http\JsonResponse;
@@ -48,7 +48,7 @@ class Api extends ResourceController
         $seasonDetails = (object) $activeSeason->toArray();
         $seasonDetails->user_season_level = $profile->season_level;
         $seasonDetails->user_season_points = $profile->season_points;
-        $seasonDetails->user_season_premium = PollUtilities::isUserSeasonPremium($profile);
+        $seasonDetails->user_season_premium = UserSeasonPremium::isUserSeasonPremium($profile);
 
         $seasonRewards = SeasonReward::where('season_id', '=', $activeSeason->id)
             ->orderBy('level')
@@ -96,7 +96,7 @@ class Api extends ResourceController
             return response()->json('Perfil del usuario no encontrado.', 404);
         }
 
-        if (!(PollUtilities::isUserSeasonPremium($profile) == true) && !($data['level'] == 1 || $data['level'] == 5 || $data['level'] == 10 || $data['level'] == 15 || $data['level'] == 20)) {
+        if (!(UserSeasonPremium::isUserSeasonPremium($profile) == true) && !($data['level'] == 1 || $data['level'] == 5 || $data['level'] == 10 || $data['level'] == 15 || $data['level'] == 20)) {
             return response()->json('No puedes canjear este nivel sin el pase de temporada premium.', 400);
         }
 
