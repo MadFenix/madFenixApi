515a9488b81b1b4853806e15fc7e225d14c1e194
Valenti Gamez <vgr.gamez@gmail.com>
2025-05-12 23:33:54 +0200

    Progreso inicial weafinity.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+use App\Models\PersonalAccessToken;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Laravel\Sanctum\Sanctum;
 
