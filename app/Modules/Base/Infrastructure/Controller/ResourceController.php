d9dec428049f74bab982db46673333c221bf989d
Valenti Gamez <vgr.gamez@gmail.com>
2025-05-14 07:07:19 +0200

    Guardado progreso inicial.

--- app/Modules/Base/Infrastructure/Controller/ResourceController.php
+++ app/Modules/Base/Infrastructure/Controller/ResourceController.php
@@ -12,6 +12,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Throwable;
