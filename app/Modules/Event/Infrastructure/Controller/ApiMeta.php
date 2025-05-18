b5577879598fc0aa4e3e2e5af50eb9b609a21c1b
Valenti Gamez <vgr.gamez@gmail.com>
2025-05-18 08:27:08 +0200

    Guardado progreso inicial.

---
+++ app/Modules/Event/Infrastructure/Controller/ApiMeta.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace App\Modules\Event\Infrastructure\Controller;
+
+use App\Modules\Base\Domain\BaseDomain;
+use App\Modules\Base\Infrastructure\Controller\ResourceController;
+use App\Modules\Blockchain\Block\Domain\BlockchainHistorical;
+use App\Modules\Event\Domain\Event;
+use App\Modules\Store\Domain\Product;
+use App\Modules\Store\Domain\ProductOrder;
+use App\Modules\User\Domain\User;
+use Carbon\Carbon;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
+
+class ApiMeta extends ResourceController
+{
+    protected function getModelName(): string
+    {
+        return 'Event\\EventMeta';
+    }
+
+    protected function getModelClass(): string
+    {
+        $modelName = $this->getModelName();
+        $lastModelName = explode('\\', $modelName);
+        $lastModelName = array_pop($lastModelName);
+
+        return '\\App\\Modules\\Event\\Domain\\' . $lastModelName;
+    }
+
+    protected function getTransformerClass(): string
+    {
+        $modelName = $this->getModelName();
+        $lastModelName = explode('\\', $modelName);
+        $lastModelName = array_pop($lastModelName);
+
+        return '\\App\\Modules\\Event\\Transformers\\' . $lastModelName;
+    }
+}
