55bff719b0cf5a2ac0c931ec6b7b94def1d0a3bc
Valenti Gamez <vgr.gamez@gmail.com>
2025-06-16 03:32:39 +0200

    Added product order resource.

--- app/Modules/Store/Transformers/ProductOrder.php
+++ app/Modules/Store/Transformers/ProductOrder.php
@@ -4,6 +4,7 @@ namespace App\Modules\Store\Transformers;
 
 use App\Modules\Base\Transformers\BaseTransformer;
 use App\Modules\Store\Domain\ProductOrder as ProductOrderModel;
+use App\Modules\User\Transformers\User;
 
 class ProductOrder extends BaseTransformer
 {
@@ -22,13 +23,11 @@ class ProductOrder extends BaseTransformer
      */
     public function toArray($request)
     {
-        $user = new \stdClass();
-        $user->id = $this->user_id;
-        $user->email = $this->user->email;
-        $user->name = $this->user->name;
         return array_merge(parent::toArray($request),[
             'product_id' => $this->product_id,
-            'user' => $user,
+            'product' => new Product($this->product),
+            'user_id' => $this->user_id,
+            'user' => new User($this->user),
             'payment_validated' => $this->payment_validated,
             'is_gift' => $this->is_gift,
             'created_at' => $this->created_at,
