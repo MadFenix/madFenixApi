06382eb86467734558450ad02a31d0515e2e6ac4
Valenti Gamez <vgr.gamez@gmail.com>
2025-06-16 03:13:33 +0200

    Added product order resource.

--- routes/api.php
+++ routes/api.php
@@ -143,9 +143,9 @@ Route::prefix('/{account}')->group(function () {
                 Route::get('/product-upload', 'Store\\Infrastructure\\Controller\\Api@uploadStatus');
                 Route::delete('/product-upload/{id}', 'Store\\Infrastructure\\Controller\\Api@deleteUpload');
 
-                Route::apiResource('product', 'Store\\Infrastructure\\Controller\\Api')->only(['index', 'destroy']);
-                Route::delete('/product', 'Store\\Infrastructure\\Controller\\Api@destroy');
-                Route::get('/product-download', 'Store\\Infrastructure\\Controller\\Api@download');
+                Route::apiResource('product-order', 'Store\\Infrastructure\\Controller\\Api')->only(['index', 'destroy']);
+                Route::delete('/product-order', 'Store\\Infrastructure\\Controller\\Api@destroy');
+                Route::get('/product-order-download', 'Store\\Infrastructure\\Controller\\Api@download');
             });
             Route::middleware('employee')->group(function () {
                 // TODO endpoints to ambassadors
