df3e0146a0081e17db82bea35399604f5ed6d136
Valenti Gamez <vgr.gamez@gmail.com>
2025-06-22 01:22:09 +0200

    Added sales data to analytics dashboard.

--- app/Modules/Game/Profile/Infrastructure/Controller/Api.php
+++ app/Modules/Game/Profile/Infrastructure/Controller/Api.php
@@ -177,8 +177,8 @@ class Api extends ResourceController
         $return = new \stdClass();
         $return->totalPlumas = Profile::sum('plumas') + Profile::sum('plumas_hedera');
         $return->totalOro = Profile::sum('oro') + Profile::sum('oro_hedera');
-        $return->totalSaleMonth = BlockchainHistorical::whereDate('created_at', '>=', Carbon::now()->startOfMonth())->where('piezas_de_oro_ft', '<', 0)->sum('piezas_de_oro_ft');
-        $return->totalSaleYear = BlockchainHistorical::whereDate('created_at', '>=', Carbon::now()->startOfMonth())->where('piezas_de_oro_ft', '<', 0)->sum('piezas_de_oro_ft');
+        $return->totalSalesMonth = BlockchainHistorical::whereDate('created_at', '>=', Carbon::now()->startOfMonth())->where('piezas_de_oro_ft', '<', 0)->sum('piezas_de_oro_ft');
+        $return->totalSalesYear = BlockchainHistorical::whereDate('created_at', '>=', Carbon::now()->startOfMonth())->where('piezas_de_oro_ft', '<', 0)->sum('piezas_de_oro_ft');
 
         $return->totalPlumasLast10Days = new \stdClass();
         $return->totalPlumasLast10Days->todayMinus0 = BlockchainHistorical::whereDate('created_at', Carbon::today())->sum('plumas');
