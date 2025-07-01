30a724d5c41ab20b14f5e8b4cb959f5b0f80028c
Valenti Gamez <vgr.gamez@gmail.com>
2025-07-01 15:26:21 +0200

    Added account settings.

--- app/Modules/User/Transformers/User.php
+++ app/Modules/User/Transformers/User.php
@@ -4,6 +4,7 @@ namespace App\Modules\User\Transformers;
 
 use App\Modules\Base\Transformers\BaseTransformer;
 use App\Modules\Event\Transformers\Event;
+use App\Modules\User\Domain\AccountSetting;
 use App\Modules\User\Domain\User as UserModel;
 
 class User extends BaseTransformer
@@ -24,6 +25,14 @@ class User extends BaseTransformer
      */
     public function toArray($request)
     {
+        $accountSettings = AccountSetting::all()->first();
+        $pgbs_used = 10;
+        $pgbs_total = 10;
+        if (!$accountSettings) {
+            $pgbs_used = $accountSettings->page_builder_sessions_used;
+            $pgbs_total = $accountSettings->page_builder_sessions_total;
+        }
+
         return [
             $this->merge(parent::toArray($request)),
             'own_events' => Event::collection($this->ownEvents),
@@ -32,6 +41,8 @@ class User extends BaseTransformer
             'ip' => $this->ip,
             'email' => $this->email,
             'name' => $this->name,
+            'page_builder_sessions_used' => $pgbs_used,
+            'page_builder_sessions_total' => $pgbs_total,
         ];
     }
 }
