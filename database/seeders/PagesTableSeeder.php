dc5a857d3471dc6eea62900a88cb93dcde34244b
Valenti Gamez <vgr.gamez@gmail.com>
2025-07-09 16:46:52 +0200

    Added seeder to new accounts.

--- database/seeders/PagesTableSeeder.php
+++ database/seeders/PagesTableSeeder.php
@@ -14,24 +14,24 @@ class PagesTableSeeder extends Seeder
      */
     public function run()
     {
-        
+
 
         \DB::table('pages')->delete();
-        
+
         \DB::table('pages')->insert(array (
-            0 => 
+            0 =>
             array (
                 'id' => 1,
                 'builder_id' => 'A956c8QYLFfMxH7l',
                 'name' => 'Home',
-            'content' => '<body><section class="text-gray-600 body-font"><div id="in7l" class="container mx-auto flex px-5 py-24 md:flex-row items-center bg-[color:var(--gris)] mb-3"><div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0"><img alt="hero" src="https://dummyimage.com/720x600" id="ij3g" class="object-cover object-center rounded"/></div><div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"><h1 id="ieg5p" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-[color:var(--blanco)]">Únete a nuestra comunidad gamming</h1><p id="ipf4f" class="mb-8 leading-relaxed text-[color:var(--blanco)]">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard <a href="https://our.welore.io/demo/p/sello_del_gamer" id="irzph">chambray</a>.</p><div class="flex justify-center"><button type="button" id="i6r95" class="inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg bg-[color:var(--naranja)]">Button</button><button type="button" class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button></div></div></div></section></body>',
+            'content' => '<body><section class="text-gray-600 body-font"><div id="in7l" class="container mx-auto flex px-5 py-24 md:flex-row items-center bg-[color:var(--gris)] mb-3"><div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0"><img alt="hero" src="https://dummyimage.com/720x600" id="ij3g" class="object-cover object-center rounded"/></div><div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center"><h1 id="ieg5p" class="title-font sm:text-4xl text-3xl mb-4 font-medium text-[color:var(--blanco)]">Únete a nuestra comunidad gamming</h1><p id="ipf4f" class="mb-8 leading-relaxed text-[color:var(--blanco)]">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p><div class="flex justify-center"><button type="button" id="i6r95" class="inline-flex text-white border-0 py-2 px-6 focus:outline-none rounded text-lg bg-[color:var(--naranja)]">Button</button><button type="button" class="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button></div></div></div></section></body>',
                 'seo_title' => 'Home',
                 'seo_description' => 'Descripción home',
                 'seo_image' => NULL,
                 'created_at' => '2025-07-01 16:21:45',
                 'updated_at' => '2025-07-01 19:52:24',
             ),
-            1 => 
+            1 =>
             array (
                 'id' => 2,
                 'builder_id' => 'LP1rdt5cA0Zkf98ko',
@@ -44,7 +44,7 @@ class PagesTableSeeder extends Seeder
                 'updated_at' => '2025-07-03 12:56:31',
             ),
         ));
-        
-        
+
+
     }
 }
